b65c0e388c13ac8c7be61edbdb456f7395d23b59
Kattni Rembor <kattni@adafruit.com>
2020-02-06 14:32:27 -0500

    Variable changes for consistency.

--- examples/clue_spirit_level.py
+++ examples/clue_spirit_level.py
@@ -5,22 +5,22 @@ from adafruit_display_shapes.circle import Circle
 import displayio
 
 display = board.DISPLAY
-group = displayio.Group(max_size=4)
+clue_group = displayio.Group(max_size=4)
 
 outer_circle = Circle(120, 120, 119, outline=clue.WHITE)
 middle_circle = Circle(120, 120, 75, outline=clue.YELLOW)
 inner_circle = Circle(120, 120, 35, outline=clue.GREEN)
-group.append(outer_circle)
-group.append(middle_circle)
-group.append(inner_circle)
+clue_group.append(outer_circle)
+clue_group.append(middle_circle)
+clue_group.append(inner_circle)
 
 x, y, _ = clue.acceleration
 bubble_group = displayio.Group(max_size=1)
 level_bubble = Circle(int(x + 120), int(y + 120), 20, fill=clue.RED, outline=clue.RED)
 bubble_group.append(level_bubble)
 
-group.append(bubble_group)
-display.show(group)
+clue_group.append(bubble_group)
+display.show(clue_group)
 
 while True:
     x, y, _ = clue.acceleration
