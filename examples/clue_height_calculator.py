42a3207bee096a4deb85066005b1660effdbf6b5
evaherrada <eva.herrada@adafruit.com>
2022-09-08 17:12:16 -0400

    Moved examples to MIT license

--- examples/clue_height_calculator.py
+++ examples/clue_height_calculator.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2019 Kattni Rembor, written for Adafruit Industries
 #
-# SPDX-License-Identifier: Unlicense
+# SPDX-License-Identifier: MIT
 """Calculate the height of an object. Press button A to reset initial height and then lift the
 CLUE to find the height."""
 from adafruit_clue import clue
